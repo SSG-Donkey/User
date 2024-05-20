1add94c8520a867bceb0fbca5754c560424056a8
giunchoe <z961223@gmail.com>
2024-05-20 15:42:22 +0900

    Revert "Redis 처리"
    This reverts commit 6a0940976a925b1d62f094cd91f2ed2f9b2702a1.

--- backend/src/main/java/com/project/backend/BackendApplication.java
+++ backend/src/main/java/com/project/backend/BackendApplication.java
@@ -3,9 +3,10 @@ package com.project.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
-
+@EnableRedisHttpSession
 public class BackendApplication {
 
     public static void main(String[] args) {
